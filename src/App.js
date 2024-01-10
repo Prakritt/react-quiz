705e78af6937fadf0ecdac790a714fd754513082
Prakrit Uprety <prakritupreety0@gmail.com>
2024-01-10 12:17:50 -0800

    Create StartScreen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 function App() {
   const initialState = {
     questions: [],
     //status can be loading, ready, error, finished
     status: "loading",
   };
-
   function reducer(state, action) {
     switch (action.type) {
       case "dataReceived":
@@ -16,14 +18,16 @@ function App() {
           questions: action.payload,
           status: "ready",
         };
-      case "dataFailed ":
+      case "dataFailed":
         return { ...state, status: "error" };
       default:
         throw new Error("Action Unknown");
     }
   }
 
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -36,8 +40,9 @@ function App() {
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
